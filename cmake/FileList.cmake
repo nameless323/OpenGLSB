1f095ca2d00632797b1d7124e4b3c68c1fb7b656
nameless <nameless323@gmail.com>
2015-09-19 21:06:58 +0300

    file list

--- cmake/FileList.cmake
+++ cmake/FileList.cmake
@@ -1,5 +1,19 @@
 set(SRC_FILES main.cpp)
 
 set(HDR_FILES
-        ${PROJECT_SOURCE_DIR}/Core/Math/SMath.h
+        ${PROJECT_SOURCE_DIR}/include/GL/gl3w.h
+        ${PROJECT_SOURCE_DIR}/include/GL/glcorearb.h
+        ${PROJECT_SOURCE_DIR}/include/GL/glext.h
+        ${PROJECT_SOURCE_DIR}/include/GL/glxext.h
+        ${PROJECT_SOURCE_DIR}/include/GL/glxext.h
+        ${PROJECT_SOURCE_DIR}/include/GL/wglext.h
+        ${PROJECT_SOURCE_DIR}/include/arcball.h
+        ${PROJECT_SOURCE_DIR}/include/object.h
+        ${PROJECT_SOURCE_DIR}/include/sb6.h
+        ${PROJECT_SOURCE_DIR}/include/sb6ext.h
+        ${PROJECT_SOURCE_DIR}/include/sb6ext.h
+        ${PROJECT_SOURCE_DIR}/include/sb6ktx.h
+        ${PROJECT_SOURCE_DIR}/include/sb6mfile.h
+        ${PROJECT_SOURCE_DIR}/include/shader.h
+        ${PROJECT_SOURCE_DIR}/include/vmath.h
 )
