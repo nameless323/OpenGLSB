79397f5ce5bea1183280a4014389e726c958b501
nameless <nameless323@gmail.com>
2015-10-06 17:57:58 +0300

    funny colors

--- MySrc/GlWindow.cpp
+++ MySrc/GlWindow.cpp
@@ -1,11 +1,23 @@
 #include <sb6.h>
+#include <cmath>
+#include <iostream>
+#include <sstream>
+
+void Log(float arg)
+{
+	std::wstringstream wss;
+	wss << arg << std::endl;
+	OutputDebugString(wss.str().c_str());
+}
 
 class GlWindow : public sb6::application
 {
 public:
 	void render(double currentTime)
 	{
-		static const GLfloat red[] = { 1.0f, 0.0f, 0.0f, 1.0f };
+		const GLfloat red[] = { sin(currentTime)*0.5f + 0.5f, cos(currentTime)*0.5f + 0.5f, 0.0f, 1.0f };
+		float val = red[0];
+		Log(val);
 		glClearBufferfv(GL_COLOR, 0, red);
 	}
 };
