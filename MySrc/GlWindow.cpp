3497f501c9cb98158bce9bcffa90e0b14a61ba94
nameless <nameless323@gmail.com>
2015-10-06 17:11:10 +0300

    at least i running

---
+++ MySrc/GlWindow.cpp
@@ -0,0 +1,13 @@
+#include <sb6.h>
+
+class GlWindow : public sb6::application
+{
+public:
+	void render(double currentTime)
+	{
+		static const GLfloat red[] = { 1.0f, 0.0f, 0.0f, 1.0f };
+		glClearBufferfv(GL_COLOR, 0, red);
+	}
+};
+
+DECLARE_MAIN(GlWindow);
