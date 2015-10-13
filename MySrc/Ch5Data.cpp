04c57c90363957e6581b65a646ed61f92f344ccb
nameless <nameless323@gmail.com>
2015-10-13 23:26:35 +0300

    set color via vertex attib ptr

--- MySrc/Ch5Data.cpp
+++ MySrc/Ch5Data.cpp
@@ -38,11 +38,21 @@ public:
 				-0.25f , -0.25f , 0.5f , 1.0f ,
 				0.25f , 0.25f , 0.5f , 1.0f
 			};
-		glGenBuffers(1, &_buffer);
-		glBindBuffer(GL_ARRAY_BUFFER, _buffer);
+		glGenBuffers(1, &_vertsBuffer);
+		glBindBuffer(GL_ARRAY_BUFFER, _vertsBuffer);
 		glBufferData(GL_ARRAY_BUFFER, 12 * sizeof(float), nullptr, GL_STATIC_DRAW);
 		glVertexAttribPointer(5, 4, GL_FLOAT, GL_FALSE, 0, nullptr);
 		glEnableVertexAttribArray(5);
+
+		const float color[] = {0.0f, 1.0f, 0.0f, 0.0f,
+			1.0f, 0.0f, 0.0f, 0.0f,
+			0.0f, 0.0f, 1.0f, 0.0f };
+		glGenBuffers(1, &_colorBuffer);
+		glBindBuffer(GL_ARRAY_BUFFER, _colorBuffer);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(color), color, GL_STATIC_DRAW);
+		glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 0, nullptr);
+		glEnableVertexAttribArray(1);
+
 		//glPointSize(40.0f);
 		//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 	}
@@ -67,8 +77,12 @@ public:
 				-0.25f , -0.25f , 0.5f , 1.0f ,
 				0.25f , 0.25f , 0.5f , 1.0f
 			};
+		//glClearBufferSubData(GL_ARRAY_BUFFER, GL_FLOAT, 0, sizeof(vertices), GL_RGBA, GL_FLOAT, vertices);
+		glBindBuffer(GL_ARRAY_BUFFER, _vertsBuffer);
 		glBufferSubData(GL_ARRAY_BUFFER, 0, 12 * sizeof(float), vertices);
-		//glBindBuffer(GL_ARRAY_BUFFER, _buffer);
+		//const float newVert[] = {-0.25f, 0.25f, 0.5f, 1.0f};
+		//glBufferSubData(GL_ARRAY_BUFFER, 12 * sizeof(float) - sizeof(newVert), sizeof(newVert), newVert);
+		//glBindBuffer(GL_ARRAY_BUFFER, _vertsBuffer);
 		glDrawArrays(GL_TRIANGLES, 0, 3);
 		//glUseProgram(0);
 	}
@@ -78,18 +92,20 @@ public:
 		glDeleteProgram(_renderingProgram);
 
 		glDisableVertexAttribArray(5);
+		glDisableVertexAttribArray(0);
 
 		glBindVertexArray(0);
 		glDeleteVertexArrays(1, &_vao);
 
 		glBindBuffer(GL_ARRAY_BUFFER, 0);
-		glDeleteBuffers(1, &_buffer);
+		glDeleteBuffers(1, &_vertsBuffer);
 	}
 
 private:
 	GLuint _renderingProgram;
 	GLuint _vao;
-	GLuint _buffer;
+	GLuint _vertsBuffer;
+	GLuint _colorBuffer;
 };
 
 DECLARE_MAIN(Ch5Data);
