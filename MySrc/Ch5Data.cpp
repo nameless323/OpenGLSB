cf7ed3df1f8f45262f3192c643c3cef87596afbc
nameless <nameless323@gmail.com>
2015-10-16 23:42:10 +0300

    shaders uniforms

--- MySrc/Ch5Data.cpp
+++ MySrc/Ch5Data.cpp
@@ -32,12 +32,6 @@ public:
 		_renderingProgram = CompileShaders1();
 		glGenVertexArrays(1, &_vao);
 		glBindVertexArray(_vao);
-		const float vertices[] =
-			{
-				0.25f , -0.25f , 0.5f , 1.0f ,
-				-0.25f , -0.25f , 0.5f , 1.0f ,
-				0.25f , 0.25f , 0.5f , 1.0f
-			};
 
 		const Vertex verts[] =
 		{
@@ -52,22 +46,17 @@ public:
 		glEnableVertexAttribArray(5);
 		glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, sizeof(Vertex), (GLvoid*)(4 * sizeof(float)));
 		glEnableVertexAttribArray(1);
-
-		//glPointSize(40.0f);
-		//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+		
 	}
 
 	void render(double currentTime)
 	{
-		//const GLfloat bckColor[] = {sin(currentTime) * 0.5f + 0.5f, cos(currentTime) * 0.5f + 0.5f, 0.0f, 1.0f};
 		const GLfloat bckColor[] = {0.0f, 0.0f, 0.0f, 1.0f};
 
 		glClearBufferfv(GL_COLOR, 0, bckColor);
 		glUseProgram(_renderingProgram);
-		//GLfloat offset[] = { sin(currentTime) * 0.5f, cos(currentTime) * 0.5f, 0.0f, 0.0f };
 		GLfloat offset[] = {0.0f, 0.0f, 0.0f, 0.0f};
 
-		//GLfloat color[] = { cos(currentTime) * 0.5f + 0.5f, sin(currentTime) * 0.5f + 0.5f, 0.0f, 1.0f };
 		GLfloat color[] = {1.0f, 1.0f, 1.0f, 1.0f};
 		glVertexAttrib4fv(0, offset);
 		glVertexAttrib4fv(1, color);
@@ -77,14 +66,8 @@ public:
 				-0.25f , -0.25f , 0.5f , 1.0f ,
 				0.25f , 0.25f , 0.5f , 1.0f
 			};
-		//glClearBufferSubData(GL_ARRAY_BUFFER, GL_FLOAT, 0, sizeof(vertices), GL_RGBA, GL_FLOAT, vertices);
-		//glBindBuffer(GL_ARRAY_BUFFER, _vertsBuffer);
-		//glBufferSubData(GL_ARRAY_BUFFER, 0, 12 * sizeof(float), vertices);
-		//const float newVert[] = {-0.25f, 0.25f, 0.5f, 1.0f};
-		//glBufferSubData(GL_ARRAY_BUFFER, 12 * sizeof(float) - sizeof(newVert), sizeof(newVert), newVert);
-		//glBindBuffer(GL_ARRAY_BUFFER, _vertsBuffer);
+		glUniform4f(2, 1.0f, 1.0f, 0.0f, 1.0f);
 		glDrawArrays(GL_TRIANGLES, 0, 3);
-		//glUseProgram(0);
 	}
 
 	void shutdown()
--- Shaders/Ch5Data.frag
+++ Shaders/Ch5Data.frag
@@ -1,9 +1,11 @@
 #version 430 core
 out vec4 color;
 
+layout (location = 2) uniform vec4 NewCol;
+
 in vec4 col;
 
 void main(void)
 {
-	color = col;
+	color = NewCol;
 }
