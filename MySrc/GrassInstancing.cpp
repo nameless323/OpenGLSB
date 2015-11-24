b275d54180b8df86d352f87c0a43a5f8daef526b
nameless <nameless323@gmail.com>
2015-11-24 23:46:14 +0300

    grass instancing

--- MySrc/GrassInstancing.cpp
+++ MySrc/GrassInstancing.cpp
@@ -28,8 +28,9 @@ public:
 		glBindBuffer(GL_ARRAY_BUFFER, _buffer);
 		glBufferData(GL_ARRAY_BUFFER, sizeof(grassBlade), grassBlade, GL_STATIC_DRAW);
 		glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, nullptr);
+		glEnableVertexAttribArray(0);
 		
-		_shaderProgram = GetShaderProgram("Shaders/GrassInstancing/GrassInstancing.frag", "Shaders/GrassInstancing/GrassInstancing.vert");
+		_shaderProgram = GetShaderProgram("Shaders/GrassInstancing/GrassInstancing.vert", "Shaders/GrassInstancing/GrassInstancing.frag");
 
 		uniforms.mvpMatrix = glGetUniformLocation(_shaderProgram, "mvpMatrix");
 
@@ -44,6 +45,37 @@ public:
 
 		glActiveTexture(GL_TEXTURE0 + 4);
 		_texGrassLen = sb6::ktx::file::load("media/textures/grass_bend.ktx");
+	}
+
+	void render(double dt) override
+	{
+		float t = (float)dt * 0.02f;
+		float r = 550.0f;
+
+		static const GLfloat black[] = { 0.0f, 0.0f, 0.0f, 1.0f };
+		static const GLfloat one = 1.0f;
+		glClearBufferfv(GL_COLOR, 0, black);
+		glClearBufferfv(GL_DEPTH, 0, &one);
+
+		vmath::mat4 mv = vmath::lookat(vmath::vec3(sinf(t) * r, 25.0f, cosf(t)*r),
+			vmath::vec3(0.0f, -50.0f, 0.0f),
+			vmath::vec3(0.0f, 1.0f, 0.0f));
+		vmath::mat4 proj = vmath::perspective(45.0f, (float)info.windowWidth / (float)info.windowHeight, 0.1f, 1000.0f);
+
+		glUseProgram(_shaderProgram);
+		glUniformMatrix4fv(uniforms.mvpMatrix, 1, GL_FALSE, proj*mv);
+		glEnable(GL_DEPTH_TEST);
+		glDepthFunc(GL_LEQUAL);
+
+		glViewport(0, 0, info.windowWidth, info.windowHeight);
+
+		glBindVertexArray(_vao);
+		glDrawArraysInstanced(GL_TRIANGLE_STRIP, 0, 6, 1024 * 1024);
+	}
+
+	void shutdown(void)
+	{
+		glDeleteProgram(_shaderProgram);
 	}
 
 private:
--- MySrc/Utils.h
+++ MySrc/Utils.h
@@ -16,15 +16,33 @@ inline GLuint GetShaderProgram(const char* vertName, const char* fragName)
 	glAttachShader(program, fragmentShader);
 	glLinkProgram(program);
 
+	GLint linkStatus;
+
+	
+	glGetProgramiv(program, GL_LINK_STATUS, &linkStatus);
+	if (linkStatus == GL_FALSE)
+	{
+		GLint len;
+		glGetProgramiv(program, GL_INFO_LOG_LENGTH, &len);
+		char* log = new char[len + 1];
+		GLsizei bytesWritten;
+		glGetProgramInfoLog(program, len, &bytesWritten, log);
+		log[len] = '\0';
+
+		OutputDebugStringA("-------------------\nShader program linking error:\n");
+		OutputDebugStringA(log);
+		OutputDebugStringA("--------------\n");
+		delete[] log;
+	}
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
 
 	return program;
 }
 
-inline void Log(float arg)
-{
-	std::wstringstream wss;
-	wss << arg << std::endl;
-	OutputDebugString(wss.str().c_str());
-}
+//inline void Log(float arg)
+//{
+//	std::wstringstream wss;
+//	wss << arg << std::endl;
+//	OutputDebugString(wss.str().c_str());
+//}
